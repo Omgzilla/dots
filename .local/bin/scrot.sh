a8dc8170cd87a69117c9cd3953f3f4bbc72a131a
Omgzilla <Ohmygodzilla@mail.com>
2024-06-26 10:33:32 +0200

    added copy to clipboard for scrot

--- .local/bin/scrot.sh
+++ .local/bin/scrot.sh
@@ -3,9 +3,9 @@ date=$(date +"%Y-%m-%d_%H-%M-%S")
 output="/home/marcus/Pictures/Screenshots/scrot-$date.png"
 
 case "$1" in
-  "select") scrot "$output" --select --line mode=edge || exit;;
-  "window") scrot "$output" --focused --border || exit;;
-  *) scrot "$output" || exit;;
+  "select") scrot "$output" --select --line mode=edge && xclip -selection clipboard -t image/png -i $output || exit;;
+  "window") scrot "$output" --focused --border && xclip -selection clipboard -t image/png -i $output || exit;;
+  *) scrot "$output" && xclip -selection clipboard -t image/png -i $output || exit;;
 esac
 
 notify-send "Screenshot taken! ($output)"
