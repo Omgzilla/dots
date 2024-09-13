de6c43dfa4a7cae01a9cf3213bb743993240bdfd
Omgzilla <Ohmygodzilla@mail.com>
2024-09-13 19:48:54 +0200

    [nvim] added some settings for markdown

--- .config/nvim/lua/omgzilla/plugins/markdown.lua
+++ .config/nvim/lua/omgzilla/plugins/markdown.lua
@@ -4,4 +4,14 @@ return {
   main = "render-markdown",
   opts = {},
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local markdown = require("render-markdown")
+
+    markdown.setup({
+      enable = true,
+      file_types = { "markdown" },
+      render_modes = { "n", "c" },
+      max_file_size = 5.0,
+    })
+  end,
 }
