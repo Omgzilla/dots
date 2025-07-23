32e9173da83ef7422f759b8bad0eeb0644d6c231
Omgzilla <Ohmygodzilla@mail.com>
2025-07-24 00:29:34 +0200

    Neovim: renamed dracula theme file, and disabled dracula

---
+++ .config/nvim/lua/omgzilla/plugins/dracula.lua
@@ -0,0 +1,12 @@
+return {
+  --  "dracula/vim",
+  --  priority = 1000,
+  --  opts = {
+  --    options = {
+  --      backgroun = "dark",
+  --    },
+  --  },
+  --  config = function()
+  --    vim.cmd([[colorscheme dracula]])
+  --  end,
+}
