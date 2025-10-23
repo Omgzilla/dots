40f253cd93f2d966bfabcd499152250fc07a1173
Omgzilla <Ohmygodzilla@mail.com>
2025-10-23 22:16:31 +0200

    tested more options for transparency
    Didn't work with Catpuccin :'(

--- .config/nvim/lua/omgzilla/plugins/dracula.lua
+++ .config/nvim/lua/omgzilla/plugins/dracula.lua
@@ -1,12 +1,13 @@
 return {
-  --  "dracula/vim",
-  --  priority = 1000,
-  --  opts = {
-  --    options = {
-  --      backgroun = "dark",
-  --    },
-  --  },
-  --  config = function()
-  --    vim.cmd([[colorscheme dracula]])
-  --  end,
+--  "dracula/vim",
+--  priority = 1000,
+--  opts = {
+--    options = {
+--      background = "dark",
+--    },
+--  },
+--  config = function()
+--    -- vim.cmd([[colorscheme dracula]])
+--    vim.cmd.colorscheme("dracula")
+--  end,
 }
