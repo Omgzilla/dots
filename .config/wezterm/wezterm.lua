c84c00b59db3e83f59b8dbd2b28e6e264d882077
Omgzilla <Ohmygodzilla@mail.com>
2025-11-10 16:01:18 +0100

    wezterm: add adjust size, rotate focus and toggle zoom for panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,13 +10,15 @@ config.initial_rows = 28
 config.bold_brightens_ansi_colors = true
 config.hide_tab_bar_if_only_one_tab = true
 config.pane_focus_follows_mouse = true
+config.cursor_blink_rate = 800
+config.text_blink_rate = 500
 
 config.enable_scroll_bar = true
 config.scrollback_lines = 100000
 config.font = wezterm.font('Ubuntu Mono', { weight = 'Light', style = 'Normal'})
 config.font_size = 10
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Catppuccin Mocha'
 config.colors = {
   scrollbar_thumb = 'rgba(86,95,137,90%)'
 }
@@ -30,6 +32,16 @@ config.keys = {
   { key = 'l', mods = 'CTRL|SHIFT', action = a.ActivatePaneDirection 'Right' },
   { key = 'k', mods = 'CTRL|SHIFT', action = a.ActivatePaneDirection 'Up' },
   { key = 'j', mods = 'CTRL|SHIFT', action = a.ActivatePaneDirection 'Down' },
+  { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = a.AdjustPaneSize { 'Left', 5 } },
+  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = a.AdjustPaneSize { 'Right', 5 } },
+  { key = 'UpArrow', mods = 'CTRL|SHIFT', action = a.AdjustPaneSize { 'Up', 5 } },
+  { key = 'DownArrow', mods = 'CTRL|SHIFT', action = a.AdjustPaneSize { 'Down', 5 } },
+  { key = 'n', mods = 'CTRL|SHIFT', action = a.RotatePanes 'Clockwise' },
+  { key = 'p', mods = 'CTRL|SHIFT', action = a.RotatePanes 'CounterClockwise' },
+  { key = 'x', mods = 'CTRL|SHIFT', action = a.TogglePaneZoomState },
 }
+-- config.mouse_bindings = {
+--  { event = { Drag = { streak = 1, button = 'Left' } }, mods = 'CTRL|SHIFT', action = a.StartWindowDrag },
+-- }
 
 return config
