ad13df8a17aeadfb2d777ff603f82aa28b007325
Omgzilla <Ohmygodzilla@mail.com>
2026-06-01 15:04:54 +0200

    update telescope for neovim

--- nvim/.config/nvim/lua/omgzilla/plugins/telescope.lua
+++ nvim/.config/nvim/lua/omgzilla/plugins/telescope.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  -- branch = "0.1.x",
+  -- tag = "v0.1.9",
+  tag = "v0.2.2",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
