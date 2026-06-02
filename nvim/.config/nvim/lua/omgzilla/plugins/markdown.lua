ef394a21571c9d7d07f46e2349db3631bbd62dae
Omgzilla <Ohmygodzilla@mail.com>
2026-06-02 14:47:53 +0200

    Nvim: added mdx support for render-markdown

--- nvim/.config/nvim/lua/omgzilla/plugins/markdown.lua
+++ nvim/.config/nvim/lua/omgzilla/plugins/markdown.lua
@@ -1,18 +1,18 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  config = function()
-    local markdown = require("render-markdown")
-    markdown.setup({
-      enabled = true,
-      render_modes = { "n", "c" }, -- modes to render
-      position = 'inline',
-      file_types = { "markdown" },
-      max_file_size = 5.0,
-      completions = {
-        lsp = { enabled = true }
-      },
-    })
-  end,
+  dependencies = { 
+    -- "nvim-treesitter/nvim-treesitter", 
+    "nvim-tree/nvim-web-devicons" 
+  },
+  ft = { "markdown", "mdx" },
+  opts = {
+    enabled = true,
+    render_modes = { "n", "c" }, -- modes to render
+    position = 'inline',
+    file_types = { "markdown", "mdx" },
+    max_file_size = 5.0,
+    completions = {
+      lsp = { enabled = true }
+    },
+  },
 }
