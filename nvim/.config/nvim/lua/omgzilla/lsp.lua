7d9b9788c783a6fe1732953b98ef9fab30e9115e
Omgzilla <Ohmygodzilla@mail.com>
2026-06-03 00:58:20 +0200

    Nvim: updated lsp config to the new lua way

--- nvim/.config/nvim/lua/omgzilla/lsp.lua
+++ nvim/.config/nvim/lua/omgzilla/lsp.lua
@@ -1,15 +1,15 @@
-local keymap = vim.keymap -- for conciseness
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
   callback = function(ev)
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf, silent = true }
+    local keymap = vim.keymap -- for conciseness
+    local builtin = require("telescope.builtin")
 
     -- set keybinds
     opts.desc = "Show LSP references"
-    keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
+    keymap.set("n", "gR", builtin.lsp_references, opts) -- show definition, references
     opts.desc = "Go to declaration"
     keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
@@ -17,10 +17,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definition
 
     opts.desc = "Show LSP implementations"
-    keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+    keymap.set("n", "gi", builtin.lsp_implementations, opts) -- show lsp implementations
 
     opts.desc = "Show LSP type definitions"
-    keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+    keymap.set("n", "gt", builtin.lsp_type_definitions, opts) -- show lsp type definitions
 
     opts.desc = "See available code actions"
     keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -29,7 +29,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
     keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
     opts.desc = "Show buffer diagnostics"
-    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+    keymap.set("n", "D", function()
+      builtin.diagnostics({ bufnr = 0 })
+    end, opts) -- show  diagnostics for file
 
     opts.desc = "Show line diagnostics"
     keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -48,7 +50,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
     keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
     opts.desc = "Restart LSP"
-    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+    keymap.set("n", "rs", function()
+      vim.cmd("LspRestart")
+    end, opts) -- mapping to restart lsp if necessary
   end,
 })
 
