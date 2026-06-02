fa22c2ea66fa24dad7ece60e98c21af49b54e030
Omgzilla <Ohmygodzilla@mail.com>
2026-06-03 01:01:25 +0200

    Nvim: updated old treesitter to work with the 0.12.*

--- nvim/.config/nvim/lua/omgzilla/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/omgzilla/plugins/nvim-treesitter.lua
@@ -1,61 +1,53 @@
 return {
---  {
---    "nvim-treesitter/nvim-treesitter",
---    event = { "BufReadPre", "BufNewFile" },
---    build = ":TSUpdate",
---    dependencies = {
---      "nvim-treesitter/nvim-treesitter-textobjects",
---      "windwp/nvim-ts-autotag",
---    },
---    config = function()
---      -- import nvim-treesitter plugin
---      local treesitter = require("nvim-treesitter.configs")
---
---      local autotag = require("nvim-ts-autotag")
---      autotag.setup({
---        -- enable autotagging (w/ nvim-ts-autotag plugin)
---        autotag = {
---          enable = true,
---        }
---      })
---      -- configure treesitter
---      treesitter.setup({ -- enable syntax highlighting
---        highlight = {
---          enable = true,
---        },
---        -- enable indentation
---        indent = { enable = true },
---        -- ensure these language parsers are installed
---        ensure_installed = {
---          "bash",
---          "css",
---          "dockerfile",
---          "gitignore",
---          "html",
---          "json",
---          "javascript",
---          "lua",
---          "markdown",
---          "markdown_inline",
---          "python",
---          "query",
---          "scss",
---          "toml",
---          "vim",
---          "yaml",
---        },
---        incremental_selection = {
---          enable = true,
---          keymaps = {
---            init_selection = "<C-space>",
---            node_incremental = "<C-space>",
---            scope_incremental = false,
---            node_decremental = "<bs>",
---          },
---        },
---      })
---      -- use bash parser for zsh files
---      vim.treesitter.language.register("bash", "zsh")
---    end,
---  },
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  dependencies = {
+    "neovim-treesitter/treesitter-parser-registry",
+  },
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter")
+
+    treesitter.setup()
+
+    treesitter.install({
+      "bash",
+      "css",
+      "dockerfile",
+      "gitignore",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "nix",
+      "python",
+      "query",
+      "scss",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    })
+
+    vim.filetype.add({
+      extension = {
+        mdx = "mdx",
+      },
+    })
+    vim.treesitter.language.register("markdown", "mdx")
+    vim.treesitter.language.register("bash", "zsh")
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("UserTreesitter", { clear = true }),
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
+    })
+
+  end,
 }
